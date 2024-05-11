c5803d0595c622b03f5ec6348015a9cb039f3944
Rijvi Islam <rijviislam54@gmail.com>
2024-05-12 05:16:12 +0600

    feat: design the error page

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -1,3 +1,22 @@
+import { Link } from "react-router-dom";
+import ErrImg from "../../assets/browser.png";
+
 export default function ErrorPage() {
-  return <div>ErrorPage</div>;
+  return (
+    <div>
+      <div className="flex flex-col gap-5 w-full h-[100vh] items-center justify-center">
+        <img
+          src={ErrImg}
+          className="w-[200px] h-[200px]"
+          alt="Page Not Fdund"
+        />
+        <h2 className="text-[#FE5D0D] text-2xl font-semibold">
+          Page Not Found
+        </h2>
+        <Link className="btn bg-[#FE5D0D] text-white" to="/">
+          Back To Home Page
+        </Link>
+      </div>
+    </div>
+  );
 }
